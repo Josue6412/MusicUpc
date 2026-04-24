971b7e1356e5581bf555c2eae3b257bcc6bd89b0
Josue6412 <u202012268@upc.edu.pe>
2026-04-23 21:06:10 -0500

    nuevo repositorio

---
+++ src/main/java/com/example/musicupc/MusicUpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicupc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicUpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicUpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musicupc/MusicUpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicupc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicUpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
